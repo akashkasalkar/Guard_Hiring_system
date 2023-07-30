e542e19026bda3f209f57bfbbf90b0d6a8b24296
akashkasalkar <kasalkar16@gmail.com>
2023-07-30 23:20:23 +0530

    intial commit

---
+++ admin/dashboard.php
@@ -0,0 +1,59 @@
+
+
+
+  <?php include_once('./header.php');?>
+
+ 
+<?php include_once('./sidebar.php');?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-4">
+          <div class="card bg-dark border border-light">
+            <div class="card-body">
+              <p class="h1 p-5"> Category
+              <span class="display-1 float-right">2</span>
+            </p>
+            </div>
+          </div>
+          </div>
+          <div class="col-4">
+          <div class="card bg-light border border-dark">
+            <div class="card-body">
+              <p class="h1 p-5"> Users
+              <span class="display-1 float-right">5</span>
+            </p>
+            </div>
+          </div>
+          </div>
+          <div class="col-4">
+          <div class="card bg-dark border border-light">
+            <div class="card-body">
+              <p class="h1 p-5"> Employee
+              <span class="display-1 float-right">20</span>
+            </p>
+            </div>
+          </div>
+          </div>
+          
+         
+        </div>
+      
+
+      </div><!-- /.container-fluid -->
+     
+    </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+  <?php include_once('./footer.php');?>
+
+ 
---
+++ admin/view_property.php
@@ -0,0 +1,95 @@
+
+
+
+<?php include_once('./header.php');?>
+
+ 
+<?php include_once('./sidebar.php');?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+        <div class="col-md-12 ">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Property Details</h3>
+              </div>
+              <div class="card-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>S.No</th>
+                  <th>Photo</th>
+                  <th>Property Name</th>
+                  <th>Property Address</th>
+                  <th>Category</th>
+                  <th>Owner Name</th>
+                  <th>Owner Mobile</th>
+
+                  <th>Action</th>
+                
+                </tr>
+                </thead>
+                <tbody>
+                <?php 
+                        $qry="select * from property p,category c where p.fk_cat_id=c.id ";
+                        $exc=mysqli_query($con,$qry);
+                        $i=1;
+                        while($row=mysqli_fetch_array($exc)) {
+                           
+                        ?> 
+                    <tr>
+                    <td><?php echo $i ?></td>
+                    <td>
+                        <img src="<?php echo $row['p_photo'] ?>" alt="" width="150">
+                    </td>
+                    <td><?php echo $row['p_name'] ?></td>
+                    <td><?php echo $row['address'] ?></td>
+
+
+                    <td><?php echo $row['cat_name'] ?></td>
+                    <td><?php echo $row['owner_name'] ?></td>
+                    <td><?php echo $row['owner_mobile'] ?></td>
+
+
+                    <td> <a href="" class="btn btn-primary">Edit</a>
+
+                    <a href="" onClick="return confirm('Do you really want to delete?');" class="btn btn-danger">Delete</a>
+
+                    </td>
+                    </tr>      
+                <?php
+                     $i++;
+                    } ?>  
+                </tbody>
+                
+              </table>
+            </div>
+          
+            </div>
+            <!-- /.card -->
+
+          </div>
+        
+          
+         
+        </div>
+      
+
+      </div><!-- /.container-fluid -->
+     
+    </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+  <?php include_once('./footer.php');?>
+
+ 
